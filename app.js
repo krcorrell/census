c24424bb805a472aa090e39139c27b657d7daa61
Kyle Correll <krcorrell2014@hotmail.com>
2016-12-16 00:31:02 +0000

    bleh2

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var routes = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_api', 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname,'app_api', 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.use(function(req, res){
   res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
@@ -37,25 +37,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next){
-    res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-  });
-}
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 // error handler
-app.use(function(err, req, res, next){
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error');
 });
-
-
 
 module.exports = app;
